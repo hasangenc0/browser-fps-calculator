4705059363802b7b052db5fa7680436986bdebec
hasan genc <hasangencx@hotmail.com>
2021-11-08 23:33:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-export default function calculateFPS() {
-    let render = 0;
-    let start = 0;
-    function step(timestamp) {
-      if (render++ === 0) {
-        start = timestamp;
-      }
+  export default function calculateFPS() {
+      return new Promise((res) => {
+        let render = 0;
+        let start = 0;
+        function step(timestamp) {
+          if (render++ === 0) {
+            start = timestamp;
+          }
 
-      if (render > 100) {
-        return Math.ceil(100000 / (timestamp - start));
-      }
+          if (render > 100) {
+            return res(Math.ceil(100000 / (timestamp - start)));
+          }
 
-      window.requestAnimationFrame(step);
-    }
+          window.requestAnimationFrame(step);
+        }
 
-    window.requestAnimationFrame(step);
-}
+        window.requestAnimationFrame(step);
+    });
+  }
